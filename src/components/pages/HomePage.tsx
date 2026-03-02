ef4db3f6cb266550984186f294a711fa47b2e122
Wix Remote Machine <support@wix.com>
2026-03-02 18:20:32 +0000

    Manual edits

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1186,7 +1186,7 @@ function DonateSection() {
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
-            className="font-heading text-6xl lg:text-8xl font-black text-maroon uppercase tracking-tight relative z-10"
+            className="font-heading text-6xl font-black text-maroon uppercase tracking-tight relative z-10 lg:text-5xl"
           >
             💛 Donation & <br/>
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-gold to-gold2">Support</span>
@@ -1523,7 +1523,7 @@ function UpcomingEventsSection() {
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
-            className="font-heading text-6xl lg:text-8xl font-black text-maroon uppercase tracking-tight relative z-10"
+            className="font-heading text-6xl font-black text-maroon uppercase tracking-tight relative z-10 lg:text-5xl"
           >
             Upcoming <br/>
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-gold to-gold2">Events</span>
