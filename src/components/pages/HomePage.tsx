35d31a373a8d6aba24227f21a5e9db7e17aa9615
Wix Remote Machine <support@wix.com>
2026-03-03 16:19:29 +0000

    Move star element up

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -314,7 +314,7 @@ function HeroSection() {
       >
         {/* Ornament Text */}
         <motion.div 
-          className="mb-6 pt-16"
+          className="mb-6 pt-8"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1.2, delay: 0.2 }}
