c5ec0687ed9c7ac5f735e324e212d3a1f4435dc3
Wix Remote Machine <support@wix.com>
2026-03-03 05:59:27 +0000

    Move hero image up

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -362,7 +362,7 @@ function HeroSection() {
 
         {/* Hero Image Box */}
         <motion.div 
-          className="mx-auto mb-12 max-w-[600px]"
+          className="mx-auto mb-6 md:mb-12 max-w-[600px] -mt-4 md:mt-0"
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 1.2, delay: 1 }}
