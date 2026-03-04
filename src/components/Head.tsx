a64cf883c7ae6fb4d24bdf4e65522cfd5e032c24
Wix Remote Machine <support@wix.com>
2026-03-04 22:59:53 +0000

    Add favicon to website

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -4,7 +4,7 @@ export const Head = () => {
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {/* Favicon */}
-      <link rel="icon" href="https://static.wixstatic.com/media/53945f_36d761dfb1fd4f9098a605fbf81d9ac7~mv2.png" type="image/png" />
+      <link rel="icon" href="https://static.wixstatic.com/media/53945f_b9c921fe89914b999237af94e34b5378~mv2.png" type="image/png" />
       {/* Fonts */}
       <link rel="preconnect" href="https://static.parastorage.com" />
     </>
