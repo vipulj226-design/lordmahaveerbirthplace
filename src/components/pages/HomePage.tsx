7ec06d4ed3090c43d30a6716998ee2d4b666bd62
Wix Remote Machine <support@wix.com>
2026-03-05 19:51:29 +0000

    Reduce Section Vertical Padding

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1533,7 +1533,7 @@ function UpcomingEventsSection() {
   };
 
   return (
-    <section id="events" ref={sectionRef} className="relative py-8 md:py-32 bg-cream overflow-hidden">
+    <section id="events" ref={sectionRef} className="relative py-4 md:py-12 bg-cream overflow-hidden">
       <div className="w-full max-w-[120rem] mx-auto px-6 lg:px-12">
         
         {/* Section Header */}
