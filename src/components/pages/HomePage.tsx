e9b00bd60c83f7a54ed36f6106c57b32ed3521b8
Wix Remote Machine <support@wix.com>
2026-03-05 19:27:22 +0000

    Move Section Further Up

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -866,7 +866,7 @@ function BlessingsSection() {
   ];
 
   return (
-    <section id="blessings" className="relative py-12 md:py-20 bg-cream overflow-hidden">
+    <section id="blessings" className="relative py-6 md:py-12 bg-cream overflow-hidden">
       <div className="w-full max-w-[120rem] mx-auto px-6 lg:px-12">
         
         {/* Section Header */}
