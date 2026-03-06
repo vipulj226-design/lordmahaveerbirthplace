0ce8abed51f98bb97411c6feeee21987be18181f
Wix Remote Machine <support@wix.com>
2026-03-06 05:41:45 +0000

    Add gap below h2

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -441,7 +441,7 @@ function BirthplaceAboutSection() {
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
-            className="font-heading text-3xl md:text-6xl font-black text-maroon uppercase tracking-tight relative z-10 leading-tight lg:text-5xl"
+            className="font-heading text-3xl md:text-6xl font-black text-maroon uppercase tracking-tight relative z-10 leading-tight lg:text-5xl mb-6 md:mb-12"
           >
             The Sacred <br/>
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-gold to-gold2 block mb-2">Birthplace</span>
