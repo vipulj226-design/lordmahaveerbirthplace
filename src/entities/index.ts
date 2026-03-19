544aa6ab71621f1b80d283561000a9b07c24db37
Wix Remote Machine <support@wix.com>
2026-03-19 19:30:30 +0000

    गैलरी CMS में मल्टी-इमेज फील्ड

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -156,6 +156,8 @@ export interface Gallery {
   _createdDate?: Date;
   _updatedDate?: Date;
   /** @wixFieldType image - Contains image URL, render with <Image> component, NOT as text */
+  galleryImages?: string;
+  /** @wixFieldType image - Contains image URL, render with <Image> component, NOT as text */
   image?: string;
   /** @wixFieldType number */
   year?: number;
