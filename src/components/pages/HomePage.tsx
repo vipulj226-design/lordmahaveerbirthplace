22f116a22316fddfafc353e523b6f04ea700f685
Wix Remote Machine <support@wix.com>
2026-03-21 07:36:19 +0000

    Increase hero image size

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -395,7 +395,7 @@ function HeroSection() {
 
         {/* Hero Image Box */}
         <motion.div 
-          className="mx-auto max-w-[280px] sm:max-w-[350px] md:max-w-[450px] w-full px-4 sm:px-0"
+          className="mx-auto max-w-[320px] sm:max-w-[380px] md:max-w-[450px] w-full px-4 sm:px-0"
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 1.2, delay: 1 }}
