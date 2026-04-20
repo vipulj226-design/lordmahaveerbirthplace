7eeefb2e1ab43875ad9da10b9687c816bee1320c
Wix Remote Machine <support@wix.com>
2026-04-20 05:07:10 +0000

    Restored to checkpoint ad1d07dd4f5146d2babd5bfa853f9fe21c16ab5f

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -108,6 +108,8 @@ export default function HomePage() {
       <GallerySection />
       {/* --- HOW TO REACH SECTION --- */}
       <HowToReachSection />
+      {/* --- DISTANCE GUIDE SECTION --- */}
+      <DistanceGuideSection />
       {/* --- CONTACT SECTION --- */}
       <ContactSection />
       {/* --- WHATSAPP CHAT BUTTON --- */}
