a25ac316f1de8636aefbf6320a813b856d5371b8
Wix Remote Machine <support@wix.com>
2026-04-27 19:49:02 +0000

    XML लिंक समस्या का निवारण करें

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,18 +1,18 @@
 export const prerender = true;
 
-export async function GET() {
-  const baseUrl = import.meta.env.SITE || 'https://example.com';
+export async function GET({ site }) {
+  const baseUrl = site?.toString() || 'https://example.com';
   
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>${baseUrl}/</loc>
+    <loc>${baseUrl}</loc>
     <lastmod>2026-04-27</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
-    <loc>${baseUrl}/gallery</loc>
+    <loc>${baseUrl}gallery</loc>
     <lastmod>2026-04-27</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
@@ -21,7 +21,7 @@ export async function GET() {
 
   return new Response(sitemap, {
     headers: {
-      'Content-Type': 'application/xml',
+      'Content-Type': 'application/xml; charset=utf-8',
       'Cache-Control': 'public, max-age=3600',
     },
   });
